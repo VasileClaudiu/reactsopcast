0852838e8df857ad1214f3ace5ca46658824ef5a
Vasile Claudiu <claudiu.vasile@gmail.com>
2019-12-14 23:49:55 +0200

    Second Commit

---
+++ precache-manifest.384d96f045b059ff9e0bc986dec16a34.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16588496caa1a2198ddb471bb0249edb",
+    "url": "/reactsopcast/index.html"
+  },
+  {
+    "revision": "02d14b8c370d46e6dae7",
+    "url": "/reactsopcast/static/css/2.138a90d2.chunk.css"
+  },
+  {
+    "revision": "f7c2ea797c1aaa0c22c6",
+    "url": "/reactsopcast/static/css/main.4b1afad1.chunk.css"
+  },
+  {
+    "revision": "02d14b8c370d46e6dae7",
+    "url": "/reactsopcast/static/js/2.538d068a.chunk.js"
+  },
+  {
+    "revision": "f7c2ea797c1aaa0c22c6",
+    "url": "/reactsopcast/static/js/main.c50ca4c9.chunk.js"
+  },
+  {
+    "revision": "e7bd92c172f3eb510dc4",
+    "url": "/reactsopcast/static/js/runtime-main.9c3de7aa.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactsopcast/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactsopcast/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactsopcast/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactsopcast/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactsopcast/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactsopcast/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactadmin/build/precache-manifest.ec9dcfa1688cd6a6e73e16bc5ada83ff.js"
+  "/reactsopcast/precache-manifest.384d96f045b059ff9e0bc986dec16a34.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactadmin/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactsopcast/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
