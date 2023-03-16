4c7c0dbc0ea363e1ce72632c55c0f7d5a1aa872f
Claudiu Vasile <claudiu.vasile@gmail.com>
2023-03-16 22:57:04 +0200

    new updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactsopcast/precache-manifest.39c6e5289150e1a8286abd798c1d2e23.js"
+  "/reactsopcast/precache-manifest.fe0c6031bc62b3c90dbded4b07a4d3ed.js"
 );
 
 workbox.clientsClaim();
